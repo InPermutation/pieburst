464131d7a56c7ea1f45910325c142215fcb5d950
Jacob Krall <jacob@jkrall.net>
2013-01-25 22:33:18 -0500

    Two piebursts, each in a g. Opts gets sumx,maxy.

--- pieburst.js
+++ pieburst.js
@@ -5,24 +5,24 @@ function pieburst (selector, data, opts) {
     function y(datum) { return datum[1]; }
 
     var def = {
-        r: 500,
+        r: 250,
         x: 250,
-        y: 250
+        y: 250,
+        maxy: d3.max(data, y),
+        sumx: d3.sum(data, x)
     };
     for(var key in def)
         opts[key] = key in opts ? opts[key] : def[key];
         
-    var sumx = data.reduce(function(p,c){ return p+x(c); }, 0),
-        maxy = data.reduce(function(p,c){ return Math.max(p, y(c)); }, 0);
     var theta = 0.0;
     var xScale = d3.scale.linear()
-                    .domain([0, sumx])
+                    .domain([0, opts.sumx])
                     .range([0, Math.PI * 2]);
     var yScale = d3.scale.linear()
-                    .domain([0, maxy])
-                    .range([0, opts.r/2]);
+                    .domain([0, opts.maxy])
+                    .range([0, opts.r]);
 
-    return d3.select(selector)
+    return d3.select(selector).append("g")
         .selectAll("path")
         .data(data)
         .enter()
